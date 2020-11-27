19dab71a883170329614dac286de5ce3184502ad
aosucas499 <aosucas499@gmail.com>
2020-11-27 21:56:26 +0100

    Add files via upload

---
+++ ubuntuminimal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wget http://centros.edu.guadalinex.org/Edu/focal/pool/main/c/cga-sourceslist-config/cga-sourceslist-config_0.3-2_all.deb
+sudo dpkg -i cga-sourceslist-config_0.3-2_all.deb 
+sudo rm *.deb
+sudo apt -o Acquire::AllowInsecureRepositories=yes update -y --allow-unauthenticated
+sudo apt -o Acquire::AllowInsecureRepositories=yes upgrade -y --allow-unauthenticated
+sudo apt -o Acquire::AllowInsecureRepositories=yes install -y --allow-unauthenticated gnome-terminal
+sudo apt -o Acquire::AllowInsecureRepositories=yes install -y --allow-unauthenticated guadalinexedu-educacion-tic
+#sudo apt -o Acquire::AllowInsecureRepositories=yes install -y --allow-unauthenticated cga-security
+#sudo apt -o Acquire::AllowInsecureRepositories=yes install -y --allow-unauthenticated ubuntu-session
+#sudo service gdm3 start
