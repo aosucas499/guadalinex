4d9e52f7491e4fbb4a3b538b1adebce80d5930bd
aosucas499 <aosucas499@gmail.com>
2020-11-28 12:02:21 +0100

    Update ubuntuminimal.sh

--- ubuntuminimal.sh
+++ ubuntuminimal.sh
@@ -2,6 +2,7 @@
 #wget http://centros.edu.guadalinex.org/Edu/focal/pool/main/c/cga-sourceslist-config/cga-sourceslist-config_0.3-2_all.deb
 #sudo dpkg -i cga-sourceslist-config_0.3-2_all.deb 
 #sudo rm *.deb
+#echo "deb [arch=amd64] http://centros.edu.guadalinex.org/Edu/focal educaandos main" > /etc/apt/sources.list
 
 sudo echo "deb [arch=amd64] http://centros.edu.guadalinex.org/Edu/focal educaandos main" > /etc/apt/sources.list.d/guadalinex.list
 sudo apt -o Acquire::AllowInsecureRepositories=yes update -y --allow-unauthenticated
@@ -11,4 +12,4 @@ sudo apt -o Acquire::AllowInsecureRepositories=yes upgrade -y --allow-unauthenti
 #sudo apt -o Acquire::AllowInsecureRepositories=yes install -y --allow-unauthenticated cga-security
 #sudo apt -o Acquire::AllowInsecureRepositories=yes install -y --allow-unauthenticated ubuntu-session
 #sudo service gdm3 start
-echo "deb [arch=amd64] http://centros.edu.guadalinex.org/Edu/focal educaandos main" > /etc/apt/sources.list
+
