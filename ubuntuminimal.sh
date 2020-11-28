78512d96706aa351bb040d122c5bdf6d0c726f74
aosucas499 <aosucas499@gmail.com>
2020-11-28 15:00:42 +0100

    Add files via upload

--- ubuntuminimal.sh
+++ ubuntuminimal.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 #wget http://centros.edu.guadalinex.org/Edu/focal/pool/main/c/cga-sourceslist-config/cga-sourceslist-config_0.3-2_all.deb
 #sudo dpkg -i cga-sourceslist-config_0.3-2_all.deb 
+wget http://centros.edu.guadalinex.org/Edu/focal/pool/main/e/educaandos-keyring/educaandos-keyring_0.3-4_amd64.deb
+sudo dpkg -i educaandos-keyring_0.3-4_amd64.deb
 #sudo rm *.deb
-#echo "deb [arch=amd64] http://centros.edu.guadalinex.org/Edu/focal educaandos main" > /etc/apt/sources.list
-
 sudo echo "deb [arch=amd64] http://centros.edu.guadalinex.org/Edu/focal educaandos main" > /etc/apt/sources.list.d/guadalinex.list
-sudo apt -o Acquire::AllowInsecureRepositories=yes update -y --allow-unauthenticated
-sudo apt -o Acquire::AllowInsecureRepositories=yes upgrade -y --allow-unauthenticated
+sudo apt update
+sudo apt upgrade
+
+
+#sudo apt -o Acquire::AllowInsecureRepositories=yes update -y --allow-unauthenticated
+#sudo apt -o Acquire::AllowInsecureRepositories=yes upgrade -y --allow-unauthenticated
 #sudo apt -o Acquire::AllowInsecureRepositories=yes install -y --allow-unauthenticated gnome-terminal
 #sudo apt -o Acquire::AllowInsecureRepositories=yes install -y --allow-unauthenticated guadalinexedu-educacion-tic
 #sudo apt -o Acquire::AllowInsecureRepositories=yes install -y --allow-unauthenticated cga-security
 #sudo apt -o Acquire::AllowInsecureRepositories=yes install -y --allow-unauthenticated ubuntu-session
 #sudo service gdm3 start
-
+#echo "deb [arch=amd64] http://centros.edu.guadalinex.org/Edu/focal educaandos main" > /etc/apt/sources.list
