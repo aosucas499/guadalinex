c9e55eb567f698045d7a10972a70b2a6193e52ba
aosucas499 <aosucas499@gmail.com>
2020-11-28 15:27:19 +0100

    Update ubuntuminimal.sh

--- ubuntuminimal.sh
+++ ubuntuminimal.sh
@@ -5,6 +5,7 @@ wget http://centros.edu.guadalinex.org/Edu/focal/pool/main/e/educaandos-keyring/
 sudo dpkg -i educaandos-keyring_0.3-4_amd64.deb
 #sudo rm *.deb
 sudo echo "deb [arch=amd64] http://centros.edu.guadalinex.org/Edu/focal educaandos main" > /etc/apt/sources.list.d/guadalinex.list
+sudo echo "deb [arch=amd64] http://centros.edu.guadalinex.org/Edu/focalsc educaandos main" >> /etc/apt/sources.list.d/guadalinex.list
 sudo apt update
 sudo apt upgrade
 
