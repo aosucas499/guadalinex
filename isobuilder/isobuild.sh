0985ecd83bdda5657329768e0dee0ab3397554c4
aosucas499 <aosucas499@gmail.com>
2020-12-20 21:23:28 +0100

    Update isobuild.sh

--- isobuilder/isobuild.sh
+++ isobuilder/isobuild.sh
@@ -4,6 +4,9 @@ sudo mv focal-sources.list /etc/apt/sources.list.d/focal.list
 sudo apt update 
 sudo apt install archdetect-deb dialog casper squashfs-tools discover aufs-tools unionfs-fuse plymouth-x11 -y
 sudo dpkg -i pinguy*
+sudo apt-get install -y ubiquity-frontend-gtk
+sudo apt-get install -f
+sudo apt-get install -y ubiquity-frontend-gtk
 sudo rm /etc/apt/sources.list.d/focal.list
 sudo apt update 
 sudo cp PinguyBuilder /usr/bin
