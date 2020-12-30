6fc10138ddf1fb0f7e92a4eab8e0afb512323edf
aosucas499 <aosucas499@gmail.com>
2020-12-30 20:22:18 +0100

    Update eggs-install.sh

--- isobuilder/eggs/eggs-install.sh
+++ isobuilder/eggs/eggs-install.sh
@@ -2,7 +2,7 @@
 sudo rm /etc/apt/sources.list.d/focal.list
 wget --no-check-certificate --content-disposition https://github.com/aosucas499/sources/raw/main/focal-sources.list
 sudo mv focal-sources.list /etc/apt/sources.list.d/focal.list
-sudo apt update 
+sudo apt-get update -y 
 wget https://sourceforge.net/projects/penguins-eggs/files/packages-deb/eggs_7.7.0-1_amd64.deb 
 sudo dpkg -i eggs*.deb
 #sudo cp install-debian.desktop /usr/lib/penguins-eggs/addons/eggs/theme/applications/
@@ -19,5 +19,5 @@ sudo cp slide.png /usr/lib/penguins-eggs/addons/eggs/theme/branding/slide7.png
 sudo eggs prerequisites
 sudo cp eggs.conf /etc/penguins-eggs.d/eggs.conf
 sudo rm /etc/apt/sources.list.d/focal.list
-sudo apt update 
+sudo apt-get update -y
 
