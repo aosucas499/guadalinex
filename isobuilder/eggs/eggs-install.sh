91e233cfb6531d719f70de91c3afb8c6f3ddfb80
aosucas499 <aosucas499@gmail.com>
2020-12-30 19:52:25 +0100

    Add files via upload

--- isobuilder/eggs/eggs-install.sh
+++ isobuilder/eggs/eggs-install.sh
@@ -3,8 +3,9 @@ sudo rm /etc/apt/sources.list.d/focal.list
 wget --no-check-certificate --content-disposition https://github.com/aosucas499/sources/raw/main/focal-sources.list
 sudo mv focal-sources.list /etc/apt/sources.list.d/focal.list
 sudo apt update 
+wget https://sourceforge.net/projects/penguins-eggs/files/packages-deb/eggs_7.7.0-1_amd64.deb 
 sudo dpkg -i eggs*.deb
-sudo install-debian.desktop cp /usr/lib/penguins-eggs/addons/eggs/theme/applications/
+sudo cp install-debian.desktop /usr/lib/penguins-eggs/addons/eggs/theme/applications/
 sudo cp show.qml /usr/lib/penguins-eggs/addons/eggs/theme/branding/show.qml
 sudo cp slide.png /usr/lib/penguins-eggs/addons/eggs/theme/branding/welcome.png
 sudo cp slide.png /usr/lib/penguins-eggs/addons/eggs/theme/branding/slide1.png
