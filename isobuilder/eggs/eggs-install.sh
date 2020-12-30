2fc2467c852ea5de4847e423114c5ebf7bbf6e5d
aosucas499 <aosucas499@gmail.com>
2020-12-30 20:21:25 +0100

    delete focal repo

--- isobuilder/eggs/eggs-install.sh
+++ isobuilder/eggs/eggs-install.sh
@@ -5,7 +5,7 @@ sudo mv focal-sources.list /etc/apt/sources.list.d/focal.list
 sudo apt update 
 wget https://sourceforge.net/projects/penguins-eggs/files/packages-deb/eggs_7.7.0-1_amd64.deb 
 sudo dpkg -i eggs*.deb
-sudo cp install-debian.desktop /usr/lib/penguins-eggs/addons/eggs/theme/applications/
+#sudo cp install-debian.desktop /usr/lib/penguins-eggs/addons/eggs/theme/applications/
 sudo cp show.qml /usr/lib/penguins-eggs/addons/eggs/theme/branding/show.qml
 sudo cp slide.png /usr/lib/penguins-eggs/addons/eggs/theme/branding/welcome.png
 sudo cp slide.png /usr/lib/penguins-eggs/addons/eggs/theme/branding/slide1.png
@@ -18,6 +18,6 @@ sudo cp slide.png /usr/lib/penguins-eggs/addons/eggs/theme/branding/slide6.png
 sudo cp slide.png /usr/lib/penguins-eggs/addons/eggs/theme/branding/slide7.png
 sudo eggs prerequisites
 sudo cp eggs.conf /etc/penguins-eggs.d/eggs.conf
-#sudo rm /etc/apt/sources.list.d/focal.list
-#sudo apt update 
+sudo rm /etc/apt/sources.list.d/focal.list
+sudo apt update 
 
