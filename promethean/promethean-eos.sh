965d8a43f54edd717092dc0ed8361a1a72ce6d69
aosucas499 <aosucas499@gmail.com>
2021-01-06 22:48:11 +0100

    # hide activinspire

--- promethean/promethean-eos.sh
+++ promethean/promethean-eos.sh
@@ -11,13 +11,15 @@
         #wget http://activsoftware.co.uk/linux/repos/driver/PrometheanLtd.asc
 	sudo apt-key add PrometheanLtd.asc
 	
-	# Paquetes necesarios
+	# Paquetes necesarios activdriver
 	#wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.4_amd64.deb
 	sudo dpkg -i libssl1.0.0_1.0.2n-1ubuntu5.4_amd64.deb
+	
+	# Paquetes necesarios activinspire
 	#wget http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu60_60.2-3ubuntu3.1_amd64.deb
-	sudo dpkg -i libicu60_60.2-3ubuntu3.1_amd64.deb
-	sudo apt-get update -y
-	sudo apt-get install gsettings-ubuntu-schemas
+	#sudo dpkg -i libicu60_60.2-3ubuntu3.1_amd64.deb
+	#sudo apt-get update -y
+	#sudo apt-get install gsettings-ubuntu-schemas
 	#sudo dpkg -i ./curl34-focal/libcurl3*.deb
 	#sudo dpkg -i ./curl34-focal/libcurl4-doc*
 	#sudo dpkg -i ./curl34-focal/libcurl4-gnutls*
@@ -26,13 +28,9 @@
 	#sudo dpkg -i ./curl34-focal/libcurl4*.deb
 	#sudo dpkg -i ./curl34-focal/curl*.deb
 	
-	
-	#Instalación de activdriver y activinspire
+	#Instalación de activdriver
 	sudo apt-get update -y
 	sudo apt install activdriver activtools -y
-	#sudo apt install activ-meta-es -y
-	#wget http://centros.edu.guadalinex.org/Edu/fenixscpdi/pool/main/a/activinspire-licence/activinspire-licence_0.1-3_all.deb
-	#sudo dpkg -i activinspire-licence_0.1-3_all.deb
 
 	#Compilación del driver para kernels 5.x
 	kernelversion=$(uname -r | awk -F. '{ print $1}')
@@ -43,6 +41,11 @@
 	echo "Compilación de drivers"
 	cd /usr/src/promethean/kernel/
 	sudo ./b
+	
+	#Instalación de activinspire
+	#wget http://centros.edu.guadalinex.org/Edu/fenixscpdi/pool/main/a/activinspire-licence/activinspire-licence_0.1-3_all.deb
+	#sudo dpkg -i activinspire-licence_0.1-3_all.deb
+	#sudo apt install activ-meta-es -y
   	
   	#Borrado de archivos
   	sudo rm -r /etc/apt/sources.list.d/promethean.list
