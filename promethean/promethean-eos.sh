d60265e34e72454e3fd3f3c7cded8b4adbe054e6
aosucas499 <aosucas499@gmail.com>
2021-01-06 22:08:40 +0100

    add focal repo

--- promethean/promethean-eos.sh
+++ promethean/promethean-eos.sh
@@ -1,26 +1,31 @@
 # Instalar drivers pizarra Promethean y programa activinspire en educaAndOs 20.04 (ubuntu focal based)
 
+	#Repos Ubuntu focal
+	sudo echo "deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ focal main universe multiverse restricted" > focal.list
+        sudo mv focal.list /etc/apt/sources.list.d/
+	
+	
 	#Repos promethean
-	#sudo echo deb [arch=amd64] http://activsoftware.co.uk/linux/repos/driver/ubuntu bionic oss non-oss > promethean.list
-        #sudo echo deb [arch=amd64] http://activsoftware.co.uk/linux/repos/ubuntu bionic non-oss >> promethean.list
-        #sudo mv promethean.list /etc/apt/sources.list.d/
+	sudo echo deb [arch=amd64] http://activsoftware.co.uk/linux/repos/driver/ubuntu bionic oss non-oss > promethean.list
+        sudo echo deb [arch=amd64] http://activsoftware.co.uk/linux/repos/ubuntu bionic non-oss >> promethean.list
+        sudo mv promethean.list /etc/apt/sources.list.d/
         #wget http://activsoftware.co.uk/linux/repos/driver/PrometheanLtd.asc
-	#sudo apt-key add PrometheanLtd.asc
+	sudo apt-key add PrometheanLtd.asc
 	
 	# Paquetes necesarios
 	#wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.4_amd64.deb
-	#sudo dpkg -i libssl1.0.0_1.0.2n-1ubuntu5.4_amd64.deb
+	sudo dpkg -i libssl1.0.0_1.0.2n-1ubuntu5.4_amd64.deb
 	#wget http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu60_60.2-3ubuntu3.1_amd64.deb
-	#sudo dpkg -i libicu60_60.2-3ubuntu3.1_amd64.deb
-	#sudo apt-get update -y
-	#sudo apt-get install gsettings-ubuntu-schemas
-	sudo dpkg -i ./curl34-focal/libcurl3*.deb
-	sudo dpkg -i ./curl34-focal/libcurl4-doc*
-	sudo dpkg -i ./curl34-focal/libcurl4-gnutls*
-	sudo dpkg -i ./curl34-focal/libcurl4*.deb
-	sudo dpkg -i ./curl34-focal/libcurl4*.deb
-	sudo dpkg -i ./curl34-focal/libcurl4*.deb
-	sudo dpkg -i ./curl34-focal/curl*.deb
+	sudo dpkg -i libicu60_60.2-3ubuntu3.1_amd64.deb
+	sudo apt-get update -y
+	sudo apt-get install gsettings-ubuntu-schemas
+	#sudo dpkg -i ./curl34-focal/libcurl3*.deb
+	#sudo dpkg -i ./curl34-focal/libcurl4-doc*
+	#sudo dpkg -i ./curl34-focal/libcurl4-gnutls*
+	#sudo dpkg -i ./curl34-focal/libcurl4*.deb
+	#sudo dpkg -i ./curl34-focal/libcurl4*.deb
+	#sudo dpkg -i ./curl34-focal/libcurl4*.deb
+	#sudo dpkg -i ./curl34-focal/curl*.deb
 	
 	
 	#Instalación de activdriver y activinspire
@@ -36,4 +41,5 @@
   	#echo "Modification du Makefile des drivers pour compatibilité avec kernels 5.*"
   	
   	#Borrado de archivos
-  	#sudo rm -r /etc/apt/sources.list.d/promethean.list
+  	sudo rm -r /etc/apt/sources.list.d/promethean.list
+  	sudo rm /etc/apt/sources.list.d/focal.list
