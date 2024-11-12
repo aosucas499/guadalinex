61bc80661b76b35d025a7aad04c84dd5c8a3970e
aosucas499 <aosucas499@gmail.com>
2024-11-12 23:57:51 +0100

    new versión cga-congelador
    Ahora hay que incluir contraseña, en caso contrario cualquier alumno puede congelar o descongelar. La contraseña es la de acceso a Gesuser, arcangel.

--- src/index.py
+++ src/index.py
@@ -1,43 +1,37 @@
 import gi
 import os
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, GLib
-import requests
-import json
-import hashlib
+from gi.repository import Gtk
 import subprocess
-
 
 class EntryWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Herramienta de congelación de usuario")
 
-	#Posicionamos la ventana en el centro de la pantalla
+        # Posicionamos la ventana en el centro de la pantalla
         self.set_position(Gtk.WindowPosition.CENTER)
 
-        #Ajustamos la dimension de la ventana
+        # Ajustamos la dimensión de la ventana
         self.set_border_width(20)
-        self.set_default_size(400,5)
+        self.set_default_size(400, 5)
 
-        #Creamos primer contenedor de forma vertical que contiene info de la aplicacion y la seleccion de radiobutton
+        # Creamos primer contenedor de forma vertical que contiene info de la aplicación y la selección de radiobutton
         self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=15)
 
         label = Gtk.Label()
-        label.set_markup("Con esta aplicación podrá congelar el estado actual del usuario genérico del \n"
-                  "sistema (configuración visual, ficheros, todo de pantalla, etc.) y dicho estado \n"
-                  "será recuperado cada vez que el usuario genérico inicie una sesión. \n"
-                  "Tenga en cuenta que si la <b>congelación</b> se encuentra <b>activada</b> todos los cambios \n"
-                  "realizados en el directorio personal del usuario genérico <b>serán eliminados,</b> \n"
-                  "incluyendo <b>los ficheros que se hayan almacenado.</b>"
+        label.set_markup(
+            "Con esta aplicación podrá congelar el estado actual del usuario genérico del \n"
+            "sistema (configuración visual, ficheros, todo de pantalla, etc.) y dicho estado \n"
+            "será recuperado cada vez que el usuario genérico inicie una sesión. \n"
+            "Tenga en cuenta que si la <b>congelación</b> se encuentra <b>activada</b> todos los cambios \n"
+            "realizados en el directorio personal del usuario genérico <b>serán eliminados,</b> \n"
+            "incluyendo <b>los ficheros que se hayan almacenado.</b>"
         )
 
         self.box.add(label)
         self.add(self.box)
 
-        label = Gtk.Label(
-            label=("Seleccione la opción a aplicar:")
-            )
-
+        label = Gtk.Label(label="Seleccione la opción a aplicar:")
         self.box.add(label)
         self.add(self.box)
 
@@ -48,8 +42,20 @@ class EntryWindow(Gtk.Window):
         self.radiobutton2.connect("toggled", self.on_radiobutton)
         self.box.pack_start(self.radiobutton2, True, True, 0)
 
-        #Creamos el tercer contenedor que contendra dos Grid con los botones de Ayuda, Aplicar y Cancelar
-        #Primer Grid que contiene el primer botón Ayuda
+        # Creamos el segundo contenedor de forma horizontal que contendrá la clave
+        self.box1 = Gtk.Box(spacing=10)
+        self.box.add(self.box1)
+        self.add(self.box)
+
+        label = Gtk.Label(label="Clave de la aplicación: ")
+        self.box1.add(label)
+
+        self.clave = Gtk.Entry()
+        self.clave.set_visibility(False)
+        self.box1.add(self.clave)
+        self.clave.connect("activate", self.on_enviar)
+
+        # Creamos el tercer contenedor que contiene los botones de Ayuda, Aplicar y Cancelar
         self.box2 = Gtk.Grid()
         self.box2.set_column_spacing(300)
         self.box.add(self.box2)
@@ -66,7 +72,7 @@ class EntryWindow(Gtk.Window):
 
         self.box2.add(ayuda)
 
-        #Segundo Grid con los botones Aplicar y Cancelar
+        # Segundo Grid con los botones Aplicar y Cancelar
         self.box3 = Gtk.Grid()
         self.box3.set_column_spacing(10)
         self.box2.add(self.box3)
@@ -74,77 +80,82 @@ class EntryWindow(Gtk.Window):
         self.box3.add(aplicar)
         self.box3.add(cancelar)
 
-    #Funcion del boton Aplicar
+    # Función del botón Aplicar
     def on_enviar(self, widget):
-        
+        texto = self.clave.get_text()
+
+        # Verificamos si la clave es "arcangel"
+        if texto == "arcangel":
             print("contraseña correcta")
-            if self.radiobutton1.get_active() == True:
-              print("congelar")
-              subprocess.run(["echo '#!/bin/sh -e\n#\n# rc.local\n#\n# This script is executed at the end of each multiuser runlevel.\n# value on error.\n#\n# In order to enable or disable this script just change the execution\n# bits.\n#\n# By default this script does nothing.' > /etc/rc.local"], shell=True)
-              # 1.- Vuelca todas las lineas del fichero a un fichero temporal menos la linea "exit 0"
-              subprocess.run(['grep -v "exit 0" /etc/rc.local > /tmp/congelar.tmp'], shell=True)
-              # 2.- Incluye la linea de sincronización al fichero temporal
-              subprocess.run(['echo "sudo rsync -a --delete /etc/.congelador/usuario/ /home/usuario/" >> /tmp/congelar.tmp'], shell=True)
-              # 3.- Incluye exit 0 al fichero temporal
-              subprocess.run(['echo "exit 0" >> /tmp/congelar.tmp'], shell=True)
-              # 4.- Copia el fichero temporal en el fichero definitivo y borra el fichero temporal
-              subprocess.run(['rm -f /etc/rc.local'], shell=True)
-              subprocess.run(['cp /tmp/congelar.tmp /etc/rc.local'], shell=True)
-              subprocess.run(['chmod 755 /etc/rc.local'], shell=True)
-              subprocess.run(['rm -f /tmp/congelar.tmp'], shell=True)
-              # Hace una copia actual del directorio personal del usuario por defecto que quedara congelado en ese momento
-              subprocess.run(['rsync -a --delete /home/usuario /etc/.congelador/'], shell=True)
+            # Aquí verificamos si está seleccionado el radiobutton de "Congelar"
+            if self.radiobutton1.get_active():
+                # Código para congelar el estado
+                print("congelar")
+                subprocess.run(["echo '#!/bin/sh -e\n#\n# rc.local\n#\n# This script is executed at the end of each multiuser runlevel.\n# value on error.\n#\n# In order to enable or disable this script just change the execution\n# bits.\n#\n# By default this script does nothing.' > /etc/rc.local"], shell=True)
+                subprocess.run(['grep -v "exit 0" /etc/rc.local > /tmp/congelar.tmp'], shell=True)
+                subprocess.run(['echo "sudo rsync -a --delete /etc/.congelador/usuario/ /home/usuario/" >> /tmp/congelar.tmp'], shell=True)
+                subprocess.run(['echo "exit 0" >> /tmp/congelar.tmp'], shell=True)
+                subprocess.run(['rm -f /etc/rc.local'], shell=True)
+                subprocess.run(['cp /tmp/congelar.tmp /etc/rc.local'], shell=True)
+                subprocess.run(['chmod 755 /etc/rc.local'], shell=True)
+                subprocess.run(['rm -f /tmp/congelar.tmp'], shell=True)
+                subprocess.run(['rsync -a --delete /home/usuario /etc/.congelador/'], shell=True)
 
-              dialog = Gtk.MessageDialog(
-              transient_for=self,
-              flags=0,
-              message_type=Gtk.MessageType.INFO,
-              buttons=Gtk.ButtonsType.OK,
-              )
-              dialog.set_title("Configuración aplicada")
-              dialog.set_markup('\n' + "El equipo se ha congelado correctamente" + '\n')
-              dialog.run()
-              print("APLICADO dialog closed")
+                dialog = Gtk.MessageDialog(
+                    transient_for=self,
+                    flags=0,
+                    message_type=Gtk.MessageType.INFO,
+                    buttons=Gtk.ButtonsType.OK,
+                )
+                dialog.set_title("Configuración aplicada")
+                dialog.set_markup('\n' + "El equipo se ha congelado correctamente" + '\n')
+                dialog.run()
+                dialog.destroy()
+                os.system("pkill -f rcLocalChecker.py")
+                os.system("python /usr/share/cga-sistema-congelacion/rcLocalChecker.py &")
 
-              dialog.destroy()
-              os.system("pkill -f rcLocalChecker.py")
-              os.system("python /usr/share/cga-sistema-congelacion/rcLocalChecker.py &")
-
-
-              self.radiobutton1.set_sensitive(False)
-              self.radiobutton2.set_sensitive(True)
+                self.radiobutton1.set_sensitive(False)
+                self.radiobutton2.set_sensitive(True)
             else:
-              print("descongelar")
-              # Elimina el comando que restaura los datos congelados del usuario por defecto
-              subprocess.run(['grep -v "sudo rsync -a --delete /etc/" /etc/rc.local > /tmp/congelar.tmp'], shell=True)
-              subprocess.run(['rm -f /etc/rc.local'], shell=True)
-              subprocess.run(['cp /tmp/congelar.tmp /etc/rc.local'], shell=True)
-              subprocess.run(['rm -f /tmp/congelar.tmp'], shell=True)
-              # Elimina los datos congelados del usuario por defecto
-              subprocess.run(['rm -rf /etc/.congelador'], shell=True)
+                # Código para descongelar el estado
+                print("descongelar")
+                subprocess.run(['grep -v "sudo rsync -a --delete /etc/" /etc/rc.local > /tmp/congelar.tmp'], shell=True)
+                subprocess.run(['rm -f /etc/rc.local'], shell=True)
+                subprocess.run(['cp /tmp/congelar.tmp /etc/rc.local'], shell=True)
+                subprocess.run(['rm -f /tmp/congelar.tmp'], shell=True)
+                subprocess.run(['rm -rf /etc/.congelador'], shell=True)
 
-              dialog = Gtk.MessageDialog(
-              transient_for=self,
-              flags=0,
-              message_type=Gtk.MessageType.INFO,
-              buttons=Gtk.ButtonsType.OK,
-              )
-              dialog.set_title("Configuración aplicada")
-              dialog.set_markup('\n' + "El equipo se ha descongelaco correctamente" + '\n')
-              dialog.run()
-              print("APLICADO dialog closed")
+                dialog = Gtk.MessageDialog(
+                    transient_for=self,
+                    flags=0,
+                    message_type=Gtk.MessageType.INFO,
+                    buttons=Gtk.ButtonsType.OK,
+                )
+                dialog.set_title("Configuración aplicada")
+                dialog.set_markup('\n' + "El equipo se ha descongelado correctamente" + '\n')
+                dialog.run()
+                dialog.destroy()
+                os.system("pkill -f rcLocalChecker.py")
+                os.system("python /usr/share/cga-sistema-congelacion/rcLocalChecker.py &")
 
-              dialog.destroy()
-              os.system("pkill -f rcLocalChecker.py")
-              os.system("python /usr/share/cga-sistema-congelacion/rcLocalChecker.py &")
+                self.radiobutton2.set_sensitive(False)
+                self.radiobutton1.set_sensitive(True)
 
+        else:
+            # Si la contraseña es incorrecta
+            print("contraseña incorrecta")
+            dialog = Gtk.MessageDialog(
+                transient_for=self,
+                flags=0,
+                message_type=Gtk.MessageType.ERROR,
+                buttons=Gtk.ButtonsType.OK,
+            )
+            dialog.set_title("Error al aplicar la configuración")
+            dialog.set_markup('\n' + " <span foreground='red'>Error: </span> La clave de la aplicación no es correcta" + '\n')
+            dialog.run()
+            dialog.destroy()
 
-              self.radiobutton2.set_sensitive(False)
-              self.radiobutton1.set_sensitive(True)
-
-          
-
-    #Funcion para el boton Ayuda
+    # Función para el botón Ayuda
     def on_info_clicked(self, widget):
         dialog = Gtk.MessageDialog(
             transient_for=self,
@@ -154,30 +165,28 @@ class EntryWindow(Gtk.Window):
             text="Ayuda",
         )
         dialog.format_secondary_text(
-            "Esta aplicación requiere para su funcionamiento de la herramienta Gesuser instalada en el servidor de contenidos del Centro Educativo.\n"
-            "Es necesario activar en Gesuser esta funcionalidad y generar la clave de activación.\n"
-            "Para obtener estos datos tendrá que consutarlo a la persona responsable de gestionar la herramienta Gesuser en su Centro Educativo."
+            "Esta aplicación ya no requiere del servidor de centro.\n"
+            "La contraseña es la misma que se utiliza para acceder al servidor de Gesuser.\n"
+            "Para obtener estos datos puede consultarlos en el canal Coordinación TDE Andalucía de Telegram  ."
         )
         dialog.run()
-        print("AYUDA cerrado correctamente")
-
         dialog.destroy()
 
-    #Funcion para el boton Cancelar
+    # Función para el botón Cancelar
     def on_cancelar(self, widget):
-      print("Aplicación cerrada")
-      Gtk.main_quit()
+        print("Aplicación cerrada")
+        Gtk.main_quit()
 
     def on_radiobutton(self, radiobutton):
         if radiobutton.get_active() == True:
-          label= "Congelar el estado del usuario genérico"
-          if radiobutton.get_label() == label:
-            print("Congelar")
-          else:
-            print("Descongelar")
-
+            label = "Congelar el estado del usuario genérico"
+            if radiobutton.get_label() == label:
+                print("Congelar")
+            else:
+                print("Descongelar")
 
 win = EntryWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
+
